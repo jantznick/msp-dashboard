e4e7dec1e87d93b955a84f01d1dc913cd9743112
jantznick <thenickjantz@gmail.com>
2025-09-15 14:57:49 -0500

    404

--- server.js
+++ server.js
@@ -80,7 +80,19 @@ app.use('/intake', intakeRoutes);
 app.use('/api', apiRoutes);
 app.use('/docs', docsRoutes);
 
+// 404 handler
+app.use((req, res, next) => {
+    res.status(404).render('error', { title: 'Not Found', message: 'The page you are looking for does not exist.' });
+});
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${port}`);
+// General error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).render('error', { title: 'Server Error', message: 'Something went wrong on our end. Please try again later.' });
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
